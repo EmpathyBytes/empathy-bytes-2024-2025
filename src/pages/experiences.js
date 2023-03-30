701a66440c8f4e1cabf7ffac5ce781149b12f20e
jamin <63824095+jamin-git@users.noreply.github.com>
2023-03-30 11:31:32 -0400

    fixed experiences

--- src/pages/experiences.js
+++ src/pages/experiences.js
@@ -40,58 +40,60 @@ function ExperiencesPage() {
         </Grid>
       </Grid>
     </Layout>
-
-
-import React from 'react';
-import ExperiencesComponent from "../components/ExperiencesComponent";
-import Navbar from "../components/navbar";
-
-const ExperiencesPage = () => {
-  return (
-    <div>
-      <Navbar transparent={false}/>
-      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: "200px", textAlign: "center" }}>
-        <ExperiencesComponent 
-          appSection={
-            <section style={{
-                width: "50%",
-                height: "550px",
-                background: "#fff",
-                boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
-                borderRadius: "10px",
-                margin: "0 50px 50px",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                textAlign: "center",
-              }}>
-              <h2>App</h2>
-              <p style={{lineHeight: "350px"}}>Here's where we'll showcase information about our app.</p>
-            </section>
-          }
-          vrSection={
-            <section style={{
-                width: "50%",
-                height: "550px",
-                background: "#fff",
-                boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
-                borderRadius: "10px",
-                margin: "0 50px 50px",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                textAlign: "center",
-              }}>
-              <h2>VR</h2>
-              <p style={{lineHeight: "350px"}}>Here's where we'll showcase information about our VR experiences.</p>
-            </section>
-          }
-        />
-      </div>
-    </div>
   );
 }
+
+
+// import React from 'react';
+// import ExperiencesComponent from "../components/ExperiencesComponent";
+// import Navbar from "../components/navbar";
+
+// const ExperiencesPage = () => {
+//   return (
+//     <div>
+//       <Navbar transparent={false}/>
+//       <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: "200px", textAlign: "center" }}>
+//         <ExperiencesComponent 
+//           appSection={
+//             <section style={{
+//                 width: "50%",
+//                 height: "550px",
+//                 background: "#fff",
+//                 boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+//                 borderRadius: "10px",
+//                 margin: "0 50px 50px",
+//                 display: "flex",
+//                 flexDirection: "column",
+//                 justifyContent: "center",
+//                 alignItems: "center",
+//                 textAlign: "center",
+//               }}>
+//               <h2>App</h2>
+//               <p style={{lineHeight: "350px"}}>Here's where we'll showcase information about our app.</p>
+//             </section>
+//           }
+//           vrSection={
+//             <section style={{
+//                 width: "50%",
+//                 height: "550px",
+//                 background: "#fff",
+//                 boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+//                 borderRadius: "10px",
+//                 margin: "0 50px 50px",
+//                 display: "flex",
+//                 flexDirection: "column",
+//                 justifyContent: "center",
+//                 alignItems: "center",
+//                 textAlign: "center",
+//               }}>
+//               <h2>VR</h2>
+//               <p style={{lineHeight: "350px"}}>Here's where we'll showcase information about our VR experiences.</p>
+//             </section>
+//           }
+//         />
+//       </div>
+//     </div>
+//   );
+// }
 
 export default ExperiencesPage;
