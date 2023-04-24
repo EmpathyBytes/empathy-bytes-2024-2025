f4d48d539943a9b7403cca46d3a08b91337a158e
carlyyin <carlyxyin@gmail.com>
2023-04-24 18:17:02 -0400

    updated contacts page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,24 +2,24 @@ import React from "react";
 import Layout from "../components/layout";
 import "../styles/contactPage.css"; // Import CSS file for contactPage
 
-const contactPage = () => {
+const ContactPage = () => {
   const handleEmailButtonClick = () => {
     window.location.href = "mailto:achung63@gatech.edu";
   };
 
   return (
-    <div style={pageStyles}>
-      <Layout style={layoutStyles}>
+    <div className="pageStyles">
+      <Layout className="layoutStyles">
         <main>
-          <div style={headingStyles}>
+          <div className="headingStyles">
             <h1>
-              <span style={headingAccentStyles}>Contact Us</span>
+              <span className="headingAccentStyles">Contact Us</span>
             </h1>
           </div>
-          <div style={centerStyles}>
-            <label style={labelStyle}>
+          <div className="centerStyles">
+            <label className="labelStyle">
               <button
-                style={button}
+                className="button"
                 type="button"
                 onClick={handleEmailButtonClick}
               >
@@ -28,11 +28,12 @@ const contactPage = () => {
             </label>
           </div>
         </main>
-        <footer style={footerStyles}>
+        <footer className="footerStyles">
         </footer>
       </Layout>
     </div>
   );
 };
 
-export default contactPage;
+export default ContactPage;
+
