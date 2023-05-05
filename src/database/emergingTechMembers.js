4b4298b5ff0d372cbeea4bc3cc4e58202ab1c6da
jamin <63824095+jamin-git@users.noreply.github.com>
2023-05-05 16:50:37 -0400

    updated emerging database

--- src/database/emergingTechMembers.js
+++ src/database/emergingTechMembers.js
@@ -37,6 +37,14 @@ const members = [
         major: "Computer Science",
         image: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
     },
+    {
+        key: 5,
+        name: "Alison Jasadipura",
+        role: "Emerging Tech Team",
+        year: "4th Year",
+        major: "Computational Media",
+        image: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+    },
 
 ];
 
