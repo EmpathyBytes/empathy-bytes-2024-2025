0c9cc5decfecc78389709b279f66f8092d59f4e5
Evelyn Craven <evelyn.mc.314@gmail.com>
2023-11-08 16:43:23 -0500

    conditional video support fixes

--- src/templates/article.js
+++ src/templates/article.js
@@ -9,34 +9,34 @@ import "../styles/articles.css";
 // This Function will find all the article components on the drupal site, and create a page
 // on the gatsby site for each component
 
-// TODO: Interviews without audio currently have the Usselman interview. Need a better solution for this.
 function Article({data}) {
     const post = data.nodeArticle;
 
     return (
         <Layout>
             <div className="articleContainer">
-            <div className="articleImageContainer">
-                <img className="articleImage" src={post.relationships.field_image.localFile.url}></img>
-                <div className="articleTitleInfo">
-                    <h1>{post.title}</h1>
-                    <h3>By {post.field_author}</h3>
-                       <audio className="articleAudio" src = {post.relationships.field_audio != null ? "https://www.empathybytes.library.gatech.edu" + post.relationships.field_audio.path.alias : "https://www.empathybytes.library.gatech.edu/sites/default/files/2023-02/Usselman-Interview-Nov.-13-2019.mp3"} controls>
-                        There is no audio for this article.
-                       </audio>
+            {post.relationships.field_audio && 
+                <div className="articleImageContainer">
+                    <img className="articleImage" src={post.relationships.field_image.localFile.url}></img>
+                    <div className="articleTitleInfo">
+                        <h1>{post.title}</h1>
+                        <h3>By {post.field_author}</h3>
+                        <audio className="articleAudio" src = {post.relationships.field_audio != null ? "https://www.empathybytes.library.gatech.edu" + post.relationships.field_audio.path.alias : null} controls>
+                            There is no audio for this article.
+                        </audio>
+                    </div>
                 </div>
-            </div>
+            }
+
+            { post.field_video_url &&
+                <iframe className="articleVideo" width="887" height="499" 
+                    src={post.field_video_url}>
+                </iframe>
+            }
 
 
             <div className="articleText" dangerouslySetInnerHTML={{ __html: post.body.processed }}></div>
             
-            { post.field_video_url ?
-                <iframe className="articleVideo" width="887" height="499" 
-                    src={post.field_video_url} title="Empathy Bytes Team Welcome" 
-                    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
-                </iframe>
-                : null
-            }
             
             <div class="clicker" tabindex="1">View Transcript</div>
             <div class="hiddendiv">
@@ -58,7 +58,7 @@ export const query = graphql`
     query($ArticleId: String!) {
         nodeArticle(id: { eq: $ArticleId }) {
         field_author
-        
+        field_video_url
         field_hg_dateline
         id
         title
