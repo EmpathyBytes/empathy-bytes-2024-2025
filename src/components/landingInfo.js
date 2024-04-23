3b1ddd7ed48010bb68f3078fb14e7e1ed2f2d9b7
AmaraWilliams <amaraajw@gmail.com>
2024-04-23 19:10:07 -0400

    fix component link to page within repo

--- src/components/landingInfo.js
+++ src/components/landingInfo.js
@@ -5,16 +5,11 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import CardMedia from '@mui/material/CardMedia';
-import teampic from "../images/homepage/teampic.jpg";
 import "../styles/components/landingInfo.css"
 import Icon from "../images/homepage/next-page-icon.png"
-import { Navigate } from "react-router-dom"
+import { Link } from "gatsby"
 
 function LandingCardInfo(props) {
-  function btnLink() {
-    window.location.href= "https://empathybytes.library.gatech.edu" + props.link
-  }
     return (
       <div className="card-wrapper">
         <Box>
@@ -29,11 +24,12 @@ function LandingCardInfo(props) {
                   </Typography>
       
                   <CardActions>
-                  
-                    <Button className="button" onClick= {() => btnLink(props.link)}>{props.btnTitle}<img src={Icon}>
-                      </img>
+                  <Link style={{ textDecoration: 'none' }} to={props.link}>
+                    <Button className="button">
+                      {props.btnTitle}
+                      <img src={Icon}></img>
                     </Button>
-                  
+                    </Link>
                       
                       
                   </CardActions>
