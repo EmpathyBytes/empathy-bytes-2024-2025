67e075e8ac111e994afb2fad352b93e3f490ae75
AmaraWilliams <amaraajw@gmail.com>
2024-05-01 23:58:19 -0400

    Add documentation for the infocard component

--- src/components/infocard.js
+++ src/components/infocard.js
@@ -9,6 +9,11 @@ import "../styles/components/infocard.css"
 import Icon from "../images/homepage/next-page-icon.png"
 import { Link } from "gatsby"
 
+/**
+ * This component is for quick infomation and a link to learn more about the related subject
+ * @param {*} props the parameter for setting the title, body, and link
+ * @returns the infocard
+ */
 function InfoCard(props) {
     return (
       <div className="card-wrapper">
