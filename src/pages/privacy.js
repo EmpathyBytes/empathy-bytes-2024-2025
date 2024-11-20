2bb58c88ac05b3a49183fef879a2c34803ee3f7c
Josh Samuel <joshua.r.samuel13@gmail.com>
2024-11-20 12:37:48 -0500

    Created privacy policy page

---
+++ src/pages/privacy.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Layout from "../components/layout";
+import "../styles/contactPage.css";
+
+function PrivacyPolicyPage() {
+
+  return (
+    <Layout>
+    </Layout>
+  );
+}
+
+export default PrivacyPolicyPage;
+
+export const Head = () => (
+  <>
+  <link rel="icon" type="image/png" href="https://educast.library.gatech.edu/static/empbytes-8c9db7ee75f110e619f7d85cb8b170c5.jpg" />
+  <title>Privacy Policy</title>
+  </>
+  )
