4861e77e0d9f8f0e2e3772c53a75143ca877fd60
xgenio3 <145805305+xgenio3@users.noreply.github.com>
2024-11-21 16:00:03 -0500

    added comments to the scrollToTop.js file

--- src/components/scrollToTop.js
+++ src/components/scrollToTop.js
@@ -1,6 +1,6 @@
-
-import React, { useEffect } from "react";
-import { useLocation } from "@reach/router";
+// This fixes the problem of links not going straight to the top of the page
+import { useEffect } from "react";
+import { useLocation } from "@reach/router"; // Gatsby equivalent of react-dom
 
 const ScrollToTop = ({ delay = 300 }) => {
   const { pathname } = useLocation();
@@ -11,7 +11,7 @@ const ScrollToTop = ({ delay = 300 }) => {
     }, delay);
 
     return () => clearTimeout(timer); // Cleanup timeout on component unmount
-  }, [pathname, 500]);
+  }, [pathname, 500]); // the delay (500) is IMPORTANT since it will occur after everything is loaded
 
   return null;
 };
