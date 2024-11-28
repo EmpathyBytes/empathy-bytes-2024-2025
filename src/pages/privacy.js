ddfe6dde1caece41330c221a23a42c5f2f8f841f
Josh Samuel <joshua.r.samuel13@gmail.com>
2024-11-28 08:31:36 -0500

    Imported and added PrivacyPolicy component to the page.

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Layout from "../components/layout";
 import "../styles/contactPage.css";
+import PrivacyPolicy from "../components/privacyPolicy";
 
 function PrivacyPolicyPage() {
 
   return (
     <Layout>
+      <PrivacyPolicy/>
     </Layout>
   );
 }
