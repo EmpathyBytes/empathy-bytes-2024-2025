602e425a338c4f87abd6d63d5d5b676474966118
Josh Samuel <joshua.r.samuel13@gmail.com>
2024-11-28 08:29:56 -0500

    Made PrivacyPolicy component which displays PDF of privacy policy

---
+++ src/components/privacyPolicy.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function PrivacyPolicy() {
+  return (
+        <iframe 
+            src="/privacy/privacyPolicy.pdf" 
+            width="100vw" 
+            height="100vh" 
+            title="Privacy Policy"
+        />
+  );
+}
+
+export default PrivacyPolicy;
