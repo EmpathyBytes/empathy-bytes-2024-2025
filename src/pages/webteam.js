bae0bc685b122ca2fe4e28c52e4af4b28e58f7b9
Josh Samuel <joshua.r.samuel13@gmail.com>
2024-12-05 14:47:22 -0500

    Updated web team banner image

--- src/pages/webteam.js
+++ src/pages/webteam.js
@@ -4,7 +4,6 @@ import React from "react";
 import Layout from "../components/layout";
 
 import technologies from "../images/gatsbyanddrupal.png";
-import Banner from "../images/experiences/WebTeamBanner.png";
 
 import Grid from '@mui/material/Grid';
 
@@ -15,7 +14,7 @@ function web() {
     return(
         <Layout>
             <title>Web Team</title>
-            <div className="top-banner" style={{backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.5)), url(${Banner})`}}>
+            <div className="top-banner" style={{backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.5)), url(https://empathybytes.library.gatech.edu/sites/default/files/hg_images/2024-12/Web_Fall_2024.JPG)`}}>
                 <h1 className="header-experiences">Web Team</h1>
             </div>
             <div className="full-container-experiences">
